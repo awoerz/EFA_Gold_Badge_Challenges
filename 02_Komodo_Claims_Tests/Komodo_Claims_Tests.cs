4c1e1b418099550bb35f563626b807a5f3118085
Adam Woerz <azreow@gmail.com>
2021-08-05 22:47:11 -0400

    Added unit test for the only method on my claim repository.

---
+++ 02_Komodo_Claims_Tests/Komodo_Claims_Tests.cs
@@ -0,0 +1,22 @@
+﻿using _02_Komodo_Claims_ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _02_Komodo_Claims_Tests
+{
+    [TestClass]
+    public class Komodo_Claims_Tests
+    {
+        [TestMethod]
+        public void AbleToAddClaimToRepo()
+        {
+            //Arrange
+            Claim newClaim = new Claim();
+            ClaimRepository newClaimRepo = new ClaimRepository();
+            //Act
+            bool claimAdded = newClaimRepo.AddClaimToRepo(newClaim);
+            //Assert
+            Assert.IsTrue(claimAdded);
+        }
+    }
+}
